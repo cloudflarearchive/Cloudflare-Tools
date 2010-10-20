008dee1b3e89e71ec92765ea2dfcd99ee03bd377
Ian Pye <ian@cloudflare.com>
2010-10-19 19:08:07 -0700

    updating messaging

--- cloudflare/cloudflare.php
+++ cloudflare/cloudflare.php
@@ -148,8 +148,39 @@ function cloudflare_conf() {
 } 
     ?>
     <div class="wrap">
-    <h2><?php _e('CloudFlare Configuration'); ?></h2>
-    <div class="narrow">
+
+    <?php if ($is_cf) { ?>
+    <h3>You are currently using CloudFlare!</h3>
+    <?php } else { ?>
+    <h3>You are not currently using CloudFlare. Sign up for CloudFlare <a href="http://www.cloudflare.com/" target="_blank">here</a>. Once you are on CloudFlare, this plugin will automatically work.</h3>
+    <?php } ?>
+
+    <h4><?php _e('CLOUDFLARE WORDPRESS PLUGIN:'); ?></h4>
+        <?php //    <div class="narrow"> ?>
+
+CloudFlare has developed a plugin for WordPress. By using the CloudFlare WordPress Plugin, you receive: 
+<ol>
+<li>Correct IP Address information for comments posted to your site</li>
+<li>Optimization of your server database (optional)</li>
+</ol>
+
+<h4>VERSION COMPATIBILITY:</h4>
+
+The plugin is compatible with WordPress version 2.8.6 and later. The plugin will not install unless you have a compatible platform.
+
+<h4>THINGS YOU NEED TO KNOW:</h4>
+
+<ol>
+<li>The main purpose of this plugin is to ensure you have no change to your originating IPs when using CloudFlare. Since CloudFlare acts a reverse proxy, connecting IPs now come from CloudFlare's range. This plugin will ensure you can continue to see the originating IP. Once you install the plugin, the IP benefit will be activated.</li>
+ 
+<li>This plugin can also help to ensure your server database is running optimally. If you are going to run the Database Optimizer associated with this plugin, then run it at a low traffic time. While the Database Optimizer is running, your site will go into Read Only mode, which means that you or your visitors will not be allowed to post. The optimizer should run quickly. Once the optimizer is done running, you will be able to post to your site again. To run the Database Optimizer, click the icon below.</li>
+
+<li>We recommend that any user on CloudFlare with WordPress use this plugin. </li>
+</ol>
+
+<h4>MORE INFORMATION ON CLOUDFLARE:</h4>
+
+CloudFlare is a service that makes websites load faster and protects sites from online spammers and hackers. Any website with a root domain (ie www.mydomain.com) can use CloudFlare. On average, it takes less than 5 minutes to sign up. You can learn more here: <a href="http://www.cloudflare.com/">CloudFlare.com</a>.
 
     <?php /**
 
@@ -174,20 +205,14 @@ function cloudflare_conf() {
 
     */ ?>
 
-    <?php if ($is_cf) { ?>
-    <h3>Congratulations! Your site is being accelerated and protected via CloudFlare.</h3>
-    <?php } else { ?>
-    <h3>Sign up your site for <a href="http://www.cloudflare.com/">CloudFlare</a> today.</h3>
-    <?php } ?>
-
-    <form action="" method="post" id="cloudflare-db" style="margin: auto; width: 400px; ">
+    <form action="" method="post" id="cloudflare-db">
     <input type="hidden" name="optimize" value="1" />
     <p class="submit">
-    <h3><label for="optimize_db"><?php _e('Make your site run even faster: '); ?></label></h3>
-    <input type="submit" name="submit" value="<?php _e('Run the optimizer'); ?>" /> (<?php _e('<a href="http://www.cloudflare.com/wiki/WordPressDBOptimizer">What is this?</a>'); ?>)</p>
+    <h3><label for="optimize_db"><?php _e('Make your site run even faster. Run the Database Optimizer now. (optional)'); ?></label></h3>
+    <input type="submit" name="submit" value="<?php _e('Run the optimizer'); ?>" /> (<?php _e('<a href="http://www.cloudflare.com/wiki/WordPressDBOptimizer">What is this?</a>'); ?>)
+    </p>
     </form>
-
-    </div>
+        <?php //    </div> ?>
     </div>
     <?php
 }
--- cloudflare/readme.txt
+++ cloudflare/readme.txt
@@ -10,11 +10,23 @@ CloudFlare integrates your blog with the CloudFlare platform.
 
 == Description ==
 
-The CloudFlare plugin integrates your blog with the CloudFlare platform.
+CloudFlare has developed a plugin for WordPress. By using the CloudFlare WordPress Plugin, you receive: 
 
-The CloudFlare platform protects and accelerates any website online. Once your website is a part of the CloudFlare community, its web traffic is routed through our intelligent global network. We automatically optimize the delivery of your web pages so your visitors get the fastest page load times and best performance. We also block threats and limit abusive bots and crawlers from wasting your bandwidth and server resources. The result: CloudFlare-powered websites see a significant improvement in performance and a decrease in spam and other attacks.
+* Correct IP Address information for comments posted to your site
 
-Learn more at [CloudFlare.com](https://www.cloudflare.com/overview.html).
+* Optimization of your server database 
+
+THINGS YOU NEED TO KNOW:
+
+* The main purpose of this plugin is to ensure you have no change to your originating IPs when using CloudFlare. Since CloudFlare acts a reverse proxy, connecting IPs now come from CloudFlare's range. This plugin will ensure you can continue to see the originating IP. 
+
+* This plugin can also help to ensure your server database is running optimally. If you are going to run the Database Optimizer associated with this plugin, then run it at a low traffic time. While the database optimizer is running, your site will go into Read Only mode, which means that you or your visitors will not be allowed to post. The optimizer should run quickly. Once the optimizer is done running, you will be able to post to your site again. 
+
+* We recommend that any user on WordPress and CloudFlare should use this plugin. 
+
+MORE INFORMATION ON CLOUDFLARE:
+
+CloudFlare is a service that makes websites load faster and protects sites from online spammers and hackers. Any website with a root domain (ie www.mydomain.com) can use CloudFlare. On average, it takes less than 5 minutes to sign up. You can learn more here: [CloudFlare.com](https://www.cloudflare.com/overview.html).
 
 == Installation ==
 
